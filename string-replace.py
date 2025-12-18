eeddaf153d3b63c1fab523d899192af51987e1ac
Tina2510 <tinachinivar@gmail.com>
2025-12-18 15:51:08 +0530

    replacing string template

---
+++ string-replace.py
@@ -0,0 +1,10 @@
+#Replacing String Template
+user_name = input("Enter your name: ")
+
+if len(user_name) >= 3:
+    print(f"Hello {user_name} , How are you?")
+else:
+    print("Username must have minimum 3 characters")    
+
+
+
